0e4187a49242c7f6492f32a792509fc4212a5e1e
Vytautas Klimavicius <vyklimavicius@hotmail.com>
2019-05-01 12:50:13 -0400

    Almost Done

--- src/App.js
+++ src/App.js
@@ -5,11 +5,52 @@ import BookList from "./containers/BookList";
 import Bookshelf from "./containers/Bookshelf";
 
 class App extends Component {
+
+  state = {
+    books: [],
+    filterBooks: []
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3005/books')
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        books: json
+      })
+    })
+  }
+
+  handleClick = (id) => {
+
+    let filterBooks = this.state.filterBooks;
+    let newArray = this.state.books.filter( book => {
+      if (filterBooks.includes(book)) {
+        return filterBooks
+      } else if (book.id === id){
+        return book
+      }
+
+    })
+
+    this.setState({
+      filterBooks: newArray
+    })
+    // console.log(newArray);
+    console.log(newArray);
+  }
+
+  handleRemove = (id) => {
+
+
+  }
+
   render() {
+
     return (
       <div className="book-container">
-        <BookList />
-        <Bookshelf />
+        <BookList books={this.state.books} handleClick={this.handleClick} />
+        <Bookshelf books={this.state.filterBooks} handleRemove={this.handleRemove}/>
       </div>
     );
   }
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,10 +1,16 @@
 import React from "react";
 
 const Book = props => {
+
+  function takeid() {
+    let id = props.book.id;
+    props.handleClick(id);
+  }
+
   return (
     <div>
-      <h2>{/*book title*/}</h2>
-      {/*book img*/}
+      <h2>{props.book.title}</h2>
+      <img src={props.book.img} onClick={takeid} />
     </div>
   );
 };
